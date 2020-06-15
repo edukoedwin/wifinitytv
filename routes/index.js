3129768110f6abbe87242e879e5340109efad409
jenniekibiri <josphatje@gmail.com>
2020-06-15 18:50:53 +0300

    create main pages

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,12 @@ router.get('/', function(req, res) {
 router.get('/about', function(req, res) {
   res.render('about', { title: 'Express' });
 });
+router.get('/stream', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/getapp', function(req, res) {
+  res.render('getApp', { title: 'Express' });
+});
+
 module.exports = router;
